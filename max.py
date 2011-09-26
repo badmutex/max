f43df15b3c9b1c2bc810a5fb06b206613ba75756
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-09-26 13:17:40 -0400

    provide Connection class wrapping zmq.socket for autmatic pickle/unpickle-ing

--- max.py
+++ max.py
@@ -12,17 +12,51 @@ import cPickle as pickle
 
 
 
+class Connection(object):
+    """
+    Wraps a ZMQ socket for automatic serialization/deserialization of messages
+    """
+
+    def __init__(self, zmq_socket):
+        self.socket = zmq_socket
+
+    def send(self, obj, *args, **kws):
+        """
+        Send the object over the zmq.socket after serialization
+
+        @param obj (object): any pickle-able python object
+        @params *args, **kws: any other arguments to pass to zqm.socket.send
+        """
+        msg = pickle.dumps(obj)
+        self.socket.send(msg, *args, **kws)
+
+    def recv(self, *args, **kws):
+        """
+        Receive a deserialzed message
+
+        @params *args, **kws: any further arguments to be passed to zmq.socket.recv
+        @return (object): an unpickled object
+        """
+
+        msg = self.socket.recv(*args, **kws)
+        obj = pickle.loads(msg)
+        return obj
+
+
+class WQMaster(object):
+    pass
+
 def worker():
 
     print 'worker: starting'
 
-    context  = zmq.Context()
-    receiver = context.socket(zmq.PULL)
-    receiver.connect('tcp://*:5559')
+    context = zmq.Context()
+    sock    = context.socket(zmq.PULL)
+    sock.connect('tcp://*:5559')
+    conn    = Connection(sock)
 
     while True:
-        msg = receiver.recv()
-        obj = pickle.loads(msg)
+        obj = conn.recv()
 
         if type(obj) is bool and not obj:
             break
@@ -35,13 +69,13 @@ def master(n):
     print 'master: starting'
 
     context = zmq.Context()
-    sender  = context.socket(zmq.PUSH)
-    sender.bind('tcp://*:5559')
+    sock  = context.socket(zmq.PUSH)
+    sock.bind('tcp://*:5559')
+    conn = Connection(sock)
 
     for i in xrange(n):
-        msg = pickle.dumps(i)
-        sender.send(msg)
-    sender.send(pickle.dumps(False))
+        conn.send(i)
+    conn.send(False)
 
 
 
