20ed3ce507d88e7addcdad41367bfbfe40e2725a
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-09-29 12:43:22 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='max',
+      version='0.1',
+      py_modules= ['max'
+                   ],
+      author = "Badi' Abdul-Wahid",
+      author_email = 'abdulwahidc@gmail.com',
+      description = '(Mapping@X) Perform first-order analysis on Data@X data'
+      )
